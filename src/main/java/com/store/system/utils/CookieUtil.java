ed52e12f8793029d1d48358013edf6013d459855
yanlele <331393627@qq.com>
2019-11-16 22:16:21 +0800

    feat: CookieUtil

---
+++ src/main/java/com/store/system/utils/CookieUtil.java
@@ -0,0 +1,48 @@
+package com.store.system.utils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CookieUtil {
+
+    // 设置cookie
+    public static void set(
+            HttpServletResponse response,
+            String name,
+            String value,
+            int maxAge
+    ) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+
+        response.addCookie(cookie);
+    }
+
+    // 获取cookie
+    public static Cookie get(
+            HttpServletRequest request,
+            String name
+    ) {
+        Map<String , Cookie>  cookieMap = readCookieMap(request);
+        if (cookieMap.containsKey(name)) {
+            return cookieMap.get(name);
+        }
+        return null;
+    }
+
+    // 封装cookie
+    private static Map<String , Cookie> readCookieMap(HttpServletRequest request) {
+        Map<String, Cookie> cookieMap = new HashMap<>();
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null ) {
+            for (Cookie cookie: cookies) {
+                cookieMap.put(cookie.getName(), cookie);
+            }
+        }
+        return cookieMap;
+    }
+}
