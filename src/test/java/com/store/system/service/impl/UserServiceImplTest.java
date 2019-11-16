b6d20dcb9dd84fe0d6ad22bafb8aaac79bf91c7b
yanlele <331393627@qq.com>
2019-11-16 23:20:29 +0800

    feat: 添加测试

---
+++ src/test/java/com/store/system/service/impl/UserServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.store.system.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void update() {
+    }
+}
